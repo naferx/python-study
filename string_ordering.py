10f22fa870851ccb3db9c852f65155015fa94ac7
naferx <nafr.snabr@gmail.com>
2023-11-22 11:18:02 -0500

    understanding unicode

--- string_ordering.py
+++ string_ordering.py
@@ -1,5 +1,14 @@
+
+'''
+Unicode:
+    0 (48), A(65), a (97)
+'''
+
+print('unicode a:', ord('a'), 'unicode A:',  ord('A'))
+print('unicode 0:', ord('0'))
+
 s1 = 'Where are the snows of yesteryear?'
 s2 = s1.split()
-s3 = sorted(s2)
-print(s3)
-print(s3[1])
+s3 = sorted(s2) #ascending order
+print(s3) # ['Where', 'are', 'of', 'snows', 'the', 'yesteryear?']
+print(s3[1]) # are
