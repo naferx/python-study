b966836a9ffedacda258bc86da0e4f219a233f42
naferx <nafr.snabr@gmail.com>
2023-11-28 21:59:55 -0500

    add conversion exercises

--- conversions.py
+++
@@ -1,4 +0,0 @@
-average = 13.2
-age = 18
-print(str(average))
-print(str(age))
--- test.py
+++
@@ -1,5 +0,0 @@
-s1 = '12.8' #ValueError: invalid literal for int() with base 10: '12.8'
-i = int(s1)
-s2 = str(i)
-f = float(s2)
-print(s1 == s2)
