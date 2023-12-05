c404e7353a0ef431f26ce51bf106216e55b29326
naferx <nafr.snabr@gmail.com>
2023-12-04 20:12:44 -0500

    class

--- classes.py
+++ classes.py
@@ -5,7 +5,18 @@ obj = TestClass()
 
 class MyStack:
     def __init__(self) -> None:
-        print('hi!')
+        self.__stack = []
 
+    def push(self, value) -> None:
+        self.__stack.append(value)
+
+    def pop(self):
+        val = self.__stack[-1]
+        del self.__stack[-1]
+        return val
+   
 
 st = MyStack()
+st.push(2)
+st.push(6)
+print(st.pop())
