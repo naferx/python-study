76838e945f16da0908caa6352d3764fbcfe4e51c
naferx <nafr.snabr@gmail.com>
2023-12-15 12:33:34 -0500

    class variable

---
+++ class_variable.py
@@ -0,0 +1,15 @@
+class Example:
+    counter = 0
+
+    def __init__(self, val = 1) -> None:
+        self.__first = val
+        Example.counter += 1
+    
+
+example_object_1 = Example()
+example_object_2 = Example(2)
+example_object_3 = Example(4)
+
+print(example_object_1.__dict__, example_object_1.counter)
+print(example_object_2.__dict__, example_object_2.counter)
+print(example_object_3.__dict__, example_object_3.counter)
