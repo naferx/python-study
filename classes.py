2d5cf6d267c4fe5dc6ecd7a96a8855260adac9a4
naferx <nafr.snabr@gmail.com>
2023-12-15 12:25:48 -0500

    sample class

--- classes.py
+++ classes.py
@@ -14,9 +14,26 @@ class MyStack:
         val = self.__stack[-1]
         del self.__stack[-1]
         return val
-   
 
-st = MyStack()
+class AddingStack(MyStack):   
+    def __init__(self) -> None:
+        MyStack.__init__(self)
+        self.__sum = 0
+
+    def get_sum(self):
+        return self.__sum
+    
+    def pop(self):
+        v = MyStack.pop(self)
+        self.__sum -= v
+        return v
+    
+    def push(self, v):
+        MyStack.push(self, v)
+        self.__sum += v
+
+
+st = AddingStack()
 st.push(2)
 st.push(6)
-print(st.pop())
+print(st.get_sum())
