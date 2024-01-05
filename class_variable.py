8f025a172f175bb10c965ecf607d540e23261bf1
naferx <nafr.snabr@gmail.com>
2024-01-05 07:15:31 -0500

    minor

--- class_variable.py
+++ class_variable.py
@@ -10,6 +10,7 @@ example_object_1 = Example()
 example_object_2 = Example(2)
 example_object_3 = Example(4)
 
+
 print(example_object_1.__dict__, example_object_1.counter)
 print(example_object_2.__dict__, example_object_2.counter)
 print(example_object_3.__dict__, example_object_3.counter)
