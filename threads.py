81d7215c357043bb8aaf1292c04e64df168fb46e
naferx <nafr.snabr@gmail.com>
2024-01-29 17:07:26 -0500

    thread join example

---
+++ threads.py
@@ -0,0 +1,26 @@
+from threading import Thread
+import time
+
+
+timeout_seconds = 3.
+
+def fn(i: int) -> int:
+    print(f'executing...: {i}')
+    time.sleep(5.)
+    print(f'finished...: {i}')
+    return i + 1
+
+def run() -> None:
+    ls = [1, 2, 3]
+    threads = []
+    for l in ls:
+        thread = Thread(target=fn, args=(l, ))
+        thread.start()
+        threads.append(thread)
+    
+    for t in threads:
+        t.join(timeout=timeout_seconds)
+
+
+if __name__ == '__main__':
+    run()
