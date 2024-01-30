c6476dbe859078c75a065e689a6330d84952cbdd
naferx <nafr.snabr@gmail.com>
2024-01-29 19:17:32 -0500

    thread timed out

--- threads.py
+++ threads.py
@@ -1,25 +1,31 @@
 from threading import Thread
 import time
 
-
-timeout_seconds = 3.
+timeout_seconds = 3
 
 def fn(i: int) -> int:
     print(f'executing...: {i}')
-    time.sleep(5.)
+    time.sleep(10)
     print(f'finished...: {i}')
     return i + 1
 
 def run() -> None:
     ls = [1, 2, 3]
     threads = []
+    
     for l in ls:
         thread = Thread(target=fn, args=(l, ))
         thread.start()
         threads.append(thread)
     
+    print('before joining threads...')
     for t in threads:
         t.join(timeout=timeout_seconds)
+
+        if t.is_alive():
+            print('thread timed out ..')
+        else:
+            print('thread has terminated')
 
 
 if __name__ == '__main__':
