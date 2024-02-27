c94616922e2d6b7e009e3abf8dbf320249b9d6ed
naferx <nafr.snabr@gmail.com>
2024-02-27 08:07:59 -0500

    float arithmetic operation

--- floats.py
+++ floats.py
@@ -7,3 +7,13 @@ print(3E8)
 print(4e5)
 print(2.3e3)
 print(4e1)
+#print(4e1.1) SyntaxError: invalid syntax
+
+print('\n=== Arithmetic operations ===')
+print(1.5 + 1.2)
+print(1.5 + 1)
+print(-1.5 + 1)
+print(-1. + 1)
+
+print(1.5 * 1)
+print(1.5 / 1.2)
