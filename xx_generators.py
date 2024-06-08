0bd3ce4bc726772e4f4e82dd9193b5a829cd74bd
Nafer Sanabria [Nord Tech] <nafr.snabr@gmail.com>
2024-06-08 08:17:39 -0500

    add custom generator

--- xx_generators.py
+++ xx_generators.py
@@ -3,5 +3,15 @@ def fn(n: int):
   for i in range(n):
     yield i # instead of return
 
+def power(n: int):
+  p = 1
+  for _ in range(n):
+    yield p
+    p *= 2
+
+
 for i in fn(4):
+  print(i)
+
+for i in power(4):
   print(i)
