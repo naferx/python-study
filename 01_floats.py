84c6f866b3985d7328254478316aad7ed75bb694
Nafer Sanabria <nafr.snabr@gmail.com>
2024-10-20 08:52:29 -0500

    add more examples to float

--- 01_floats.py
+++ 01_floats.py
@@ -1,4 +1,21 @@
+
+print('\nRepresenting float values')
 average = 4.3
 
 print(average) # 4.3
 print(type(average)) # <class 'float'>
+
+print(3.2) # 3.2
+print(.8) # 0.8
+print(5.) # 5.0
+print(1_000.37) # 1000.37
+print(-21.4) # 21.4
+
+print(3e2) # 300.0
+print(4E3) # 4000.0
+print(5.2E2) # 520.0
+
+# print(4e1.1)
+#     print(4e1.1) SyntaxError: invalid syntax
+#          ^^^^^
+#SyntaxError: invalid syntax. Perhaps you forgot a comma?
