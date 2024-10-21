c660e33fa574fef5775d93fc4db7a0d891cfd412
Nafer Sanabria <nafr.snabr@gmail.com>
2024-10-20 22:52:48 -0500

    float: comments

--- 01_floats.py
+++ 01_floats.py
@@ -1,5 +1,6 @@
-
+# floating point number is positive or negative number containing one or more decimals
 print('\nRepresenting float values')
+
 average = 4.3
 
 print(average) # 4.3
@@ -11,7 +12,7 @@ print(5.) # 5.0
 print(1_000.37) # 1000.37
 print(-21.4) # 21.4
 
-# using scientific notation
+# using scientific notation, 'e' to indicate the power of 10
 print(3e2) # 300.0
 print(4E3) # 4000.0
 print(5.2E2) # 520.0
